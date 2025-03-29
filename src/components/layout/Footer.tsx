e51bf2d0daa4eacd15fdf18c0517d6e3dc05b9c0
Amir mohammad <am.sanei79@gmail.com>
2025-03-29 07:23:34 +0330

    Add footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,47 @@
-import Link from "next/link";
-
-
 export default function Footer() {
   return (
-    <footer className="flex justify-center">
-   
-  </footer>
-  )
+    <footer className="flex justify-between">
+      <div className="flex gap-2">
+        <div className="bg-white size-8 rounded-full flex items-center justify-center p-1 ">
+          <svg
+            className="size-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.3"
+              d="M12 5V3m0 18v-2M7.05 7.05 5.636 5.636m12.728 12.728L16.95 16.95M5 12H3m18 0h-2M7.05 16.95l-1.414 1.414M18.364 5.636 16.95 7.05M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"
+            />
+          </svg>
+        </div>
+        <div className="bg-white size-8 rounded-full flex items-center justify-center p-1 ">
+          <svg
+            className="size-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.3"
+              d="m13 19 3.5-9 3.5 9m-6.125-2h5.25M3 7h7m0 0h2m-2 0c0 1.63-.793 3.926-2.239 5.655M7.5 6.818V5m.261 7.655C6.79 13.82 5.521 14.725 4 15m3.761-2.345L5 10m2.761 2.655L10.2 15"
+            />
+          </svg>
+        </div>
+      </div>
+      <div className="text-xs text-black/40">11</div>
+    </footer>
+  );
 }
