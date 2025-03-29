3fd91c948f8ba613df0985ec204a4432da4dc753
Amir mohammad <am.sanei79@gmail.com>
2025-03-29 07:16:36 +0330

    Change layout

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center">
-        <div className="flex flex-col">
-          <div className="font-bold text-3xl">Amir Mohammad Sanei</div>
-          <div>designer && developer</div>
+      <div className="flex gap-2 items-center">
+        <div className="size-8 bg-white text-black font-bold flex justify-center items-center rounded">
+          11
         </div>
-        <div className="size-12 bg-black text-white p-1 text-xl">11</div>
-      </header>
-  )
+        <div>Amir Mohammad Sanei</div>
+      </div>
+      <div className="flex gap-4">
+        {LINKS.map((link) => (
+          <div key={link.id}>
+            <Link href={link.href} className=" text-sm letter tracking-widest">
+             {link.text}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </header>
+  );
 }
+
+const LINKS = [
+  { id: 1, text: "Home", href: "/" },
+  { id: 2, text: "About", href: "/about" },
+  { id: 3, text: "Skills", href: "/skills" },
+  { id: 4, text: "Projects", href: "/projects" },
+];
