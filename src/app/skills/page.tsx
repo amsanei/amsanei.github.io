c9f00a1e930b201f620259d1bc709e9982b7c96f
Amir mohammad <am.sanei79@gmail.com>
2025-03-30 06:36:15 +0330

    Fix bug

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -211,7 +211,7 @@ const DATA = [
       <svg viewBox="0 0 128 128">
         <path
           fill="#77c1d2"
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M98.444 35.562 126 62.997 98.444 90.432 70.889 62.997z"
           clipRule="evenodd"
         ></path>
@@ -219,7 +219,7 @@ const DATA = [
           fill="#2d3441"
           fillRule="evenodd"
           d="m29.556 35.562 57.126 56.876H31.571L2 62.997z"
-          clip-rule="evenodd"
+          clipRule="evenodd"
         ></path>
       </svg>
     ),
